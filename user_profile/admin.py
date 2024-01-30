bf569e4609a92fedebbf5dae27cb958547cd81c3
Yemo <113440465+yemoalakija@users.noreply.github.com>
2024-01-30 04:01:37 +0000

    Register User and Follow models with admin site

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,4 +1,6 @@
+"""This module registers the User and Follow models with the admin site."""
 from django.contrib import admin
+from .models import User, Follow
 
 # Register your models here.
 admin.site.register(User)
