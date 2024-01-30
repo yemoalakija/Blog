a3f163390608cc6a5dc0c00e0321022468b11017
Yemo <113440465+yemoalakija@users.noreply.github.com>
2024-01-30 01:28:57 +0000

    Update ALLOWED_HOSTS in settings.py

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pzs^bnkeu-0t2#*fulfg$2k^kb5il4hx(_mo=*=68qg=1s2+s+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
